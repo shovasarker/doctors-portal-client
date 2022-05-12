1a8dd29468a865b3e35669dbbb987044f0a1760f
ShovaSarker <shovasarker01@gmail.com>
2022-05-12 20:33:43 +0600

    Two empty pages Home and About is Created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/home/Home'
 import Header from './pages/shared/header/Header'
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ src/pages/About/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className='container px-6'>
+      <h1 className='text-3xl font-nedium text-center text-neutral my-10'>
+        This is About Page
+      </h1>
+    </main>
+  )
+}
+
+export default About
